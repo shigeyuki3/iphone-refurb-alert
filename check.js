58fe1437e287fa976b6e490c02bcc2a4b20255ee
shigeyuki3 <67683236+shigeyuki3@users.noreply.github.com>
2026-03-10 17:04:05 +0900

    Update check.js

--- check.js
+++ check.js
@@ -3,6 +3,8 @@ const URL="https://www.apple.com/jp/shop/refurbished/iphone"
 const TOKEN=process.env.LINE_CHANNEL_ACCESS_TOKEN
 const USER=process.env.LINE_USER_ID
 
+const fs=require("fs")
+
 async function notify(text){
 
  await fetch("https://api.line.me/v2/bot/message/push",{
@@ -24,11 +26,33 @@ async function run(){
  const res=await fetch(URL)
  const html=await res.text()
 
- if(html.includes("Air")){
+ const regex=/iPhone\s?Air[^\<]{0,80}/gi
+ const matches=[...html.matchAll(regex)].map(v=>v[0])
 
-  await notify("整備済みに iPhone Air が追加された可能性があります\nhttps://www.apple.com/jp/shop/refurbished/iphone")
+ if(matches.length===0){
+  console.log("iPhone Airなし")
+  return
+ }
+
+ let previous=[]
+
+ if(fs.existsSync("cache.json")){
+  previous=JSON.parse(fs.readFileSync("cache.json"))
+ }
+
+ const newItems=matches.filter(v=>!previous.includes(v))
+
+ if(newItems.length){
+
+  await notify(
+   "整備済みに iPhone Air が追加されました\n\n"+
+   newItems.join("\n")+
+   "\n\nhttps://www.apple.com/jp/shop/refurbished/iphone"
+  )
 
  }
+
+ fs.writeFileSync("cache.json",JSON.stringify(matches))
 
 }
 
